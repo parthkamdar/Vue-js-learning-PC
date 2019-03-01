c69f5d74d1d0d12b4155a1c07320adc9d1958010
parthkamdar <parth.kamdar@gmail.com>
2019-03-01 12:00:59 +0530

    First Commit
    Complete Second Course Project

---
+++ second-course-project/src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="container">
+    <app-new-quote @quoteAdded="newQuote"></app-new-quote>
+    <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
+    <div class="row alert-info">
+      <p>Info: Click on the box to delete it</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import QuoteGrid from './components/QuoteGrid.vue';
+import NewQuote from './components/NewQuote';
+export default {
+  data: function(){
+    return{
+      maxQuote: 10,
+      quotes: [
+        'this is a sample quote'
+      ]
+    }
+  },
+  components:{
+    appQuoteGrid: QuoteGrid,
+    appNewQuote: NewQuote
+  },
+  methods:{
+    newQuote(quote){
+      this.quotes.push(quote);
+    },
+    deleteQuote(index){
+      this.quotes.splice(index,1);
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.alert-info{
+  background-color: black;
+  color: #f1f1f1;
+  border-radius: 10px;
+  width: 70%;
+  display: block;
+  margin: auto;
+  height: 50px;
+  line-height: 50px;
+  text-align: center;
+}
+</style>
---
+++ second-course-project/src/components/Quote.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="col-6">
+    <div class="panel">
+      <div class="panel-body quote">
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+
+</script>
+<style>
+  .panel-body{
+    background: #ddd;
+    width: 200px;
+    height: 100px;
+    padding: 10px;
+    margin: 10px;
+    text-align: center;
+  }
+  .quote{
+    cursor: pointer;
+  }
+</style>
